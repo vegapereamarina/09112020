d26913b37c7678878284d8fabf18d89acdba8c2f
vegapereamarina <72079350+vegapereamarina@users.noreply.github.com>
2020-11-23 16:44:47 +0100

    Update consultas.js

--- src/consultas.js
+++ src/consultas.js
@@ -1,23 +1,20 @@
-//animales con peso menor a 1000T
-db.dinosaurios.find({"tamaño.pesoToneladas": {$gt:4}})
+//animales con peso menor a 500T
+db.dinosaurios.find({"tamaño.pesoToneladas": {$gt:500}})
 
-//todos los animales cuyo año sea 12000 y cuya dieta no sea omnívora (uso de $and explícito, $eq y $ne)
-db.dinosaurios.find({$and: [ {años: {$eq: 12000}} , {dieta:{$ne: "omnívoro"}} ]})
-
-//Todos los animales que no son ni carnívoros ni herbívoros (uso de $nin)
+//Todos los animales que no son ni carnívoros ni herbívoros 
 db.dinosaurios.find({dieta: {$nin: ["carnívoro", "herbívoro"]} })
 
-//Todos los animales que no son ni omnívoros ni herbívoros (uso de $not y $in)
+//Todos los animales que no son ni omnívoros ni herbívoros 
 db.dinosaurios.find({dieta: {$not: {$in: ["omnívoro" , "herbívoro"]}} })
 
-//Todos los animales que pesen entre 9 y 1 T y los que tengan una altura superior a 5 m 
-//(uso de $or, $and(implícito), $gte, $lte y $gt)
+//todos los animales cuyo año sea 12000 y cuya dieta no sea omnívora
+db.dinosaurios.find({$and: [ {años: {$eq: 12000}} , {dieta:{$ne: "omnívoro"}} ]})
+
+//Todos los animales que pesen entre 9 y 1 T y los que tengan una altura mayor que 5 m //
 db.dinosaurios.find({$or: [ {"tamaño.pesoToneladas":{$gte:1 , $lte:9}} , {"tamaño.alturaMetros":{$gt:5}} ] })
 
-//todos los animales que no vivan menos de 10000 años ni cuyo nombre acabe por 'o' (uso de $nor, $lt y $regex)
+//todos los animales que no vivan menos de 10000 años ni cuyo nombre acabe por 'o'
 db.dinosaurios.find({$nor: [{años:{$lt:10000}} , {tipo: {$regex:/o$/}}]})
 
-
-//Todos los animales que tenga en su nombre una e despues cualquier letra y despues una i,
-//un ejemplo de este tipo de palabra sería 'casa' ($uso de $regex)
+//Todos los animales que tenga en su nombre una e y una i//
 db.dinosaurios.find({tipo: {$regex:/e.i/} })
